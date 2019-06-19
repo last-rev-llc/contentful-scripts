01c46c5f7ccd0869284bf20acc4cd2f889570fca
Brad Taylor <bradtaylorsf@gmail.com>
2019-06-19 12:51:36 +0200

    try catch

--- unlinked-content/archive.js
+++ unlinked-content/archive.js
@@ -7,19 +7,23 @@ const itemCounter = 0;
   const contentfulClient = await contentful.clientWithEnv();
   const ids = await csv.idArray(path.join(__dirname, './', 'ids.csv'));
   for (let i = 0; i < ids.length; i++) {
-    const id = ids[i];
-    const entry = await contentfulClient.getEntry(id);
-    if(entry.isPublished()) {
-      console.log('UNPUBLISHED: ', entry.sys.id);
-      await entry.unpublish();
+    try {
+      const id = ids[i];
+      const entry = await contentfulClient.getEntry(id);
+      if(entry.isPublished()) {
+        console.log('UNPUBLISHED: ', entry.sys.id);
+        await entry.unpublish();
+      }
+      if(!entry.isArchived()) {
+        console.log('ARCHIVED: ', entry.sys.id);
+        await entry.archive();
+      } else {
+        console.log('ALREADY ARCHIVED: ', entry.sys.id);
+      }
+      timing.delay(200);
+    } catch (error) {
+      console.log(error);
     }
-    if(!entry.isArchived()) {
-      console.log('ARCHIVED: ', entry.sys.id);
-      await entry.archive();
-    } else {
-      console.log('ALREADY ARCHIVED: ', entry.sys.id);
-    }
-    timing.delay(200);
   }
   console.log('DONE!');
 })();
